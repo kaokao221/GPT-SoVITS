4c912ccd8544dda055915cee99944ea33f60fb3d
kaokao221 <88539021+kaokao221@users.noreply.github.com>
2025-06-09 22:16:08 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -1973,7 +1973,7 @@ with gr.Blocks(title="GPT-SoVITS WebUI", analytics_enabled=False, js=js, css=css
     app.queue().launch(  # concurrency_count=511, max_size=1022
         server_name="0.0.0.0",
         inbrowser=True,
-        share=is_share,
+        share=True,
         server_port=webui_port_main,
         # quiet=True,
     )
